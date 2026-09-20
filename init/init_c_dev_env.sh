fa3e44fa330c4b34c72432b4b4931dac227888f7
XIAN-SHENG-576692 <chenxiansheng576692@gmail.com>
2026-09-21 00:01:08 +0800

    feat(init_c_dev_env.sh): Install `frama-c`.

--- init/init_c_dev_env.sh
+++ init/init_c_dev_env.sh
@@ -17,15 +17,56 @@ REPO_SCRIPTS_DIR="${REPO_ROOT_DIR}/scripts"
 
 # ==================================================
 # Install packages
+PACKAGES=""
+
+# common
 PACKAGES="
+${PACKAGES}
+autoconf
 build-essential
 clang
 clang-format
 clang-tidy
 cmake
+gcc
 gdb
+graphviz
 libclang-dev
+make
+opam
+"
+
+# gmp
+PACKAGES="
+${PACKAGES}
+gmp
+gmp-devel
+libgmp-dev
+"
+
+# pkgconf
+PACKAGES="
+${PACKAGES}
+pkg-config
+pkgconf-pkg-config
+"
+
+# zlib
+PACKAGES="
+${PACKAGES}
+zlib1g-dev
+zlib-devel
 "
 
 "${REPO_SCRIPTS_DIR}/install_packages_cross_platform.sh" \
     "${PACKAGES}"
+
+# ==================================================
+# Install opam packages
+opam init -a -y --disable-sandboxing
+eval $(opam env)
+
+opam switch create ocaml-base-compiler
+
+# Install Frama-C
+opam install frama-c -y --assume-depexts
